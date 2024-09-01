ff29a02df3eb67b301a6e9c9b695f296c55998c9
igorcampos-dev <igorccampos8@gmail.com>
2024-09-01 00:50:22 -0300

    refactor: melhorias no readme

--- src/main/java/com/io/loopit/paysheet/exception/global/CustomExceptionHandler.java
+++ src/main/java/com/io/loopit/paysheet/exception/global/CustomExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.io.loopit.paysheet.exception.global;
 
+import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.io.loopit.paysheet.exception.Error;
 import com.nexus.aws.exception.FileAlreadyExistsException;
 import com.nexus.aws.exception.FileNotExists;
@@ -113,5 +114,12 @@ public class CustomExceptionHandler {
         log.error("method=IllegalArgumentException | message: {}", e.getMessage());
         return this.response( e.getMessage(), HttpStatus.BAD_REQUEST, s.getRequestURI());
     }
+
+    @ExceptionHandler(AmazonS3Exception.class)
+    public ResponseEntity<Error> amazonS3Exception(HttpServletRequest s, AmazonS3Exception e){
+        log.error("method=AmazonS3Exception | message: {}", e.getMessage());
+        return this.response( e.getMessage(), HttpStatus.BAD_REQUEST, s.getRequestURI());
+    }
+
 
 }
